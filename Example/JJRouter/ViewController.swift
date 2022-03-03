192a634891ff3971c7228549c63736b7a397215d
Jezz <lijie250@gmail.com>
2022-03-03 23:35:57 +0800

    Init project

---
+++ Example/JJRouter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  JJRouter
+//
+//  Created by pearjie250 on 03/03/2022.
+//  Copyright (c) 2022 pearjie250. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/JJRouter/JJRouter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JJRouter : NSObject
+@end
+@implementation PodsDummy_JJRouter
+@end
